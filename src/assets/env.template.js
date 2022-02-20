d6a3b6fc0725e905a6b19499f6c7b9190122548b
cyrill10 <cyrill.bader@gmx.ch>
2022-02-20 18:20:52 +0100

    fix template

--- src/assets/env.template.js
+++ src/assets/env.template.js
@@ -2,6 +2,6 @@
   window["env"] = window["env"] || {};
 
   // Environment variables
-  window["env"]["auth.username"] = "${AUTH.USERNAME}";
-  window["env"]["auth.password"] = "${AUTH.PASSWORD}";
+  window["env"]["auth.username"] = "${AUTH_USERNAME}";
+  window["env"]["auth.password"] = "${AUTH_PASSWORD}";
 })(this);
