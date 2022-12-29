46ea6d5493f9891b8b67a393b02bb8e3ff7fceb6
cyrill10 <cyrill.bader@gmx.ch>
2022-12-29 20:31:28 +0100

    update angular material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,10 @@ import { Component, Inject } from '@angular/core';
 
 import { Platform } from '@ionic/angular';
 import {
-  MAT_DIALOG_DATA,
-  MatDialog,
-  MatDialogRef,
-} from '@angular/material/dialog';
+  MAT_LEGACY_DIALOG_DATA as MAT_DIALOG_DATA,
+  MatLegacyDialog as MatDialog,
+  MatLegacyDialogRef as MatDialogRef,
+} from '@angular/material/legacy-dialog';
 
 import { StorageService } from './services/storage.service';
 import { Store } from '@ngrx/store';
